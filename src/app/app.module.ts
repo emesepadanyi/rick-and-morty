d040de57bebd32cbb562fefb846ed0f853226745
Emese Padanyi <emesepadany@gmail.com>
2021-01-22 20:49:46 +0100

    Add header component as menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LocationsComponent } from './locations/locations.component';
 import { CharactersComponent } from './characters/characters.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LocationsComponent,
-    CharactersComponent
+    CharactersComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
