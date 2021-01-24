6be42d8fe94c14f603385a3dd677e0afae7160fd
emesepadanyi <emesepadanyi@gmail.com>
2021-01-24 18:39:47 +0100

    Install ngx-pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -21,7 +22,8 @@ import { CharacterCardComponent } from './character-card/character-card.componen
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPaginationModule
   ],
   providers: [CharactersService],
   bootstrap: [AppComponent]
